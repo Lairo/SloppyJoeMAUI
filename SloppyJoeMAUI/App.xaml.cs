f607a2f964b671dcd00ff485a6e10569f017e09d
chajus1 <chajus1@googlemail.com>
2024-09-19 00:51:08 +0100

    Add project files.

---
+++ SloppyJoeMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SloppyJoeMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SloppyJoeMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SloppyJoeMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
